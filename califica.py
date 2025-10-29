8dd7342566193d6e984e026c720d71a7c064cd73
marcomnrq <63822742+marcomnrq@users.noreply.github.com>
2025-10-28 22:21:35 -0500

    Cambio Malo

--- califica.py
+++ califica.py
@@ -2,7 +2,7 @@
 def evaluar_cliente(edad: int, ingresos: float) -> dict:
   if edad > 18 and ingresos > 1000:
     if ingresos < 5000:
-      return {"aprobado": True, "categoria": "Tarjeta OH", "linea": 2000}
+      return {"aprobado": True, "categoria": "Tarjeta OH Premium", "linea": 2000}
 
     if ingresos >= 5000 and edad >= 25:
       return {"aprobado": True, "categoria": "Tarjeta OH Premium", "linea": 10000}
