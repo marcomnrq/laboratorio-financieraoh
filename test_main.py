d00a97a2589a925f4d9778dd7366af2e2b10d193
marcomnrq <63822742+marcomnrq@users.noreply.github.com>
2025-11-06 20:15:35 -0500

    Cambiar unit tests a nueva ruta

--- test_main.py
+++ test_main.py
@@ -9,16 +9,16 @@ def test_main_route():
   assert response.json() == {"mensaje": "Hola mundo"}
 
 def test_endpoint_datos_invalidos():
-  response = client.get("/evaluaciones/tarjetas?edad=-34&ingresos=-94")
+  response = client.get("/evaluar/tarjetas?edad=-34&ingresos=-94")
   assert response.status_code == 400
   assert response.json()["detail"] == "Datos inválidos"
 
 def test_endpoint_rechazo():
-  response = client.get("/evaluaciones/tarjetas?edad=17&ingresos=2000")
+  response = client.get("/evaluar/tarjetas?edad=17&ingresos=2000")
   assert response.status_code == 200
   assert response.json()["status"] == "RECHAZADO"
 
 def test_endpoint_regular():
-  response = client.get("/evaluaciones/tarjetas?edad=21&ingresos=2000")
+  response = client.get("/evaluar/tarjetas?edad=21&ingresos=2000")
   assert response.status_code == 200
   assert response.json()["status"] == "APRO"
